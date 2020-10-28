11166bc1fe2ae370d6a44f93d5b20c1a074e1e6f
Aariz Merchant <merchantaariz@gmail.com>
2020-10-28 19:47:43 +0000

    Initial todo list

---
+++ todo.js
@@ -0,0 +1,29 @@
+$(function(){
+    $("#add").submit(event => {
+        event.preventDefault();
+        console.log(event);
+        console.log($(event.target));
+        let form = $(event.target);
+        let item = form.children("input").val();
+        let li = $("<li></li>");
+
+        li.append("<input type='checkbox' />");
+
+        let span = $("<span></span>");
+        span.text(item);
+        li.append(span);
+        
+        
+        let button = $("<button>Delete</button>");
+        button.click(event =>{
+            //console.log("deleting");
+            console.log(event.target);
+            let toDelete = $(event.target).parent().remove();
+            console.log(toDelete);
+        });
+        li.append(button);
+        
+        $("#list").append(li);
+        form.children("input").val("");
+    });
+});

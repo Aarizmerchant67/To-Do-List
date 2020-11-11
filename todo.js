fbb311c705e5674a90d0b4930fa48b8ff128418b
Aariz Merchant <merchantaariz@gmail.com>
2020-11-11 19:33:48 +0000

    Added Github login option

--- todo.js
+++ todo.js
@@ -1,28 +1,30 @@
 var currentUser;
-$(function(){
-//     database.ref("/todo").once('value').then(function(snapshot){
-//         snapshot.forEach(function(child){
-//             let item = child.val();
-//             console.log(item);
-//             createTodo(item.text, item.done, database.ref("/todo/" + child.key));
-//             counter ++;
-//         });
-//         //console.log(items);
-//         // for(let i = 0; i < items.length; i++) {
-//         //     if (items[i]){
-//         //         createTodo(items[i].text, items[i].done);
-//         //     }
-//         //     counter ++;
-//         // } 
-//     });
 
-    // database.ref("/todo/").on("child_added", function(snapshot){
+$(function(){
+    // database.ref("/todo").once('value').then(function(snapshot){
+    //     snapshot.forEach(function(child){
+    //         let item = child.val();
+    //         console.log(item);
+    //         createTodo(item.text, item.done, database.ref("/todo/" + child.key));
+    //         counter ++;
+    //     });
+    //     // console.log(items);
+    //     // for(let i = 0; i < items.length; i++) {
+    //     //     if (items[i]){
+    //     //         createTodo(items[i].text, items[i].done);
+    //     //     }
+    //     //     counter ++;
+    //     // }
+    // });
+
+    // database.ref("/todo").on('child_added', function(snapshot){
     //     let item = snapshot.val();
     //     console.log("realtime", item);
     //     createTodo(item.text, item.done, database.ref("/todo/" + snapshot.key));
     // });
+    // // database.ref("/todo").off('child_added');
 
-    // database.ref("todo").on('child_removed', function(snapshot){
+    // database.ref("/todo").on('child_removed', function(snapshot){
     //     let id = snapshot.key;
     //     $("#" + id).parent().parent().remove();
     // });
@@ -34,7 +36,7 @@ $(function(){
     //     $("#" + snapshot.key).attr("checked", checked);
     // });
 
-    //Handle add button
+    // Handle add button
     $("#add").submit(event => {
         event.preventDefault();
         console.log(event);
@@ -44,34 +46,32 @@ $(function(){
         console.log(item);
 
         if (currentUser) {
-            let ref = database.ref("/todo/" + currentUser.uid).push();
-       
-        // createTodo(item, false, ref);
-        $("#adding").val("");
+            let ref = database.ref("/todo/"+currentUser.uid).push();
 
-        
-        ref.set({text:item, done:false});
+            // createTodo(item, false, ref);
+            $("#adding").val("");
+
+            ref.set({text:item, done:false});
         }
-        
     });
 
-$("#confirm-signup").click(event =>{
-    let email = $("#signup-email").val();
-    let pwd = $("#signup-pwd").val();
-    let confirm = $("#signup-pwd-confirm").val();
-    if (pwd !== confirm) {
-        alert("Your password and confirmation are different!")
-        return;
-    }
-    auth.createUserWithEmailAndPassword(email, pwd).then(function (result){
-        console.log(result);
-        $("#modal").modal('hide');
-    }).catch()(function(error){
-        alert(error.message);
+    $("#confirm-signup").click(event => {
+        let email = $("#signup-email").val();
+        let pwd = $("#signup-pwd").val();
+        let confirm = $("#signup-pwd-confirm").val();
+        if (pwd !== confirm) {
+            alert("Your password and confirmation don't match!");
+            return;
+        }
+        auth.createUserWithEmailAndPassword(email, pwd).then(function(result){
+            console.log(result);
+            $("#modal").modal('hide');
+        }).catch(function(error){
+            alert(error.message);
+        });
     });
-});
 
-    $("#confirm-login").click(event =>{
+    $("#confirm-login").click(event => {
         let email = $("#email").val();
         let password = $("#pwd").val();
         auth.signInWithEmailAndPassword(email, password).then(function(result){
@@ -82,73 +82,70 @@ $("#confirm-signup").click(event =>{
         });
     });
 
-    $("#google").click(event =>{
+    $("#google").click(event => {
         auth.signInWithPopup(google).then(function(result){
             console.log(result);
-             $("#modal").modal('hide');
+            $("#modal-login").modal('hide');
         }).catch(function(error){
             alert(error.message);
         });
     });
 
-    // $("#github").click(event =>{
-    //     auth.signInWithPopup(github).then(function(result){
-    //         console.log(result);
-    //          $("#modal").modal('hide');
-    //     }).catch(function(error){
-    //         alert(error.message);
-    //     });
-    // });
+    $("#github").click(event => {
+        auth.signInWithPopup(github).then(function(result){
+            console.log(result);
+            $("#modal-login").modal('hide');
+        }).catch(function(error){
+            alert(error.message);
+        });
+    });
 
     auth.onAuthStateChanged(function(user){
-       if (user) {
-        console.log("We're signed in!", user);
-        $("#signout-form").show();
-        $("#signin-form").hide();
-        $("#login-message").hide();
-        $("#list").show();
-        $("#add").show();
-        // $("#starter-text").hide();
+        if (user) {
+            console.log("We're signed in!", user);
+            $("#signout-form").show();
+            $("#signin-form").hide();
+            $("#login-message").hide();
+            $("#list").show();
+            $("#add").show();
 
-        let ref = database.ref("/todo/" + user.uid);
-        ref.on("child_added", function(snapshot){
-            let item = snapshot.val();
-            console.log("realtime", item);
-            createTodo(item.text, item.done, ref.child(snapshot.key));
-        });
-    
-        ref.on('child_removed', function(snapshot){
-            let id = snapshot.key;
-            $("#" + id).parent().parent().remove();
-        });
-    
-        ref.on('child_changed', function(snapshot){
-            // console.log("change", snapshot.val());
-            // console.log("id", snapshot.key);
-            let checked = snapshot.val().done;
-            $("#" + snapshot.key).attr("checked", checked);
-        });
-    
-       } else{
-            console.log("No one signed in!")
+            let ref = database.ref("/todo/"+user.uid);
+            ref.on('child_added', function(snapshot){
+                let item = snapshot.val();
+                console.log("realtime", item);
+                createTodo(item.text, item.done, ref.child(snapshot.key));
+            });
+            // database.ref("/todo").off('child_added');
+
+            ref.on('child_removed', function(snapshot){
+                let id = snapshot.key;
+                $("#" + id).parent().parent().remove();
+            });
+
+            ref.on('child_changed', function(snapshot){
+                // console.log("change", snapshot.val());
+                // console.log("id", snapshot.key);
+                let checked = snapshot.val().done;
+                $("#" + snapshot.key).attr("checked", checked);
+            });
+        } else {
+            console.log("No one is signed in!");
             $("#signout-form").hide();
             $("#signin-form").show();
             $("#login-message").show();
             $("#list").hide().empty();
             $("#add").hide();
-            // $("#starter-text").show();
-            let ref = database.ref("/todo/" + currentUser.uid);
+            let ref = database.ref("/todo/"+currentUser.uid);
             ref.off('child_added');
             ref.off('child_removed');
             ref.off('child_changed');
-
-       }
-       currentUser = user;
+        }
+        currentUser = user;
     });
 
-    $("#signout-btn").click(event =>{
+    $("#signout-btn").click(event => {
         auth.signOut();
-    })
+    });
 });
 
 function createTodo(item, checked, ref){
@@ -156,7 +153,6 @@ function createTodo(item, checked, ref){
     let col1 = $("<div class='custom-control custom-checkbox'></div>");
     li.append(col1);
 
-   
     let id = ref.key;
     let input = $("<input type='checkbox' class='custom-control-input' id='" + id + "' />")
     input.change(function(e){
@@ -165,7 +161,7 @@ function createTodo(item, checked, ref){
     });
     input.attr('checked', checked);
     col1.append(input);
-    
+
     let label = $("<label class='custom-control-label ml-1'></label>");
     label.attr("for", id);
     label.text(item);
@@ -173,7 +169,7 @@ function createTodo(item, checked, ref){
 
     let col2 = $("<div class='col-2'></div>");
     li.append(col2);
-    
+
     let button = $("<button class='btn btn-danger w-100'>Delete</button>");
     button.click(event => {
         // console.log("deleting");
@@ -182,7 +178,6 @@ function createTodo(item, checked, ref){
         ref.remove();
     });
     col2.append(button);
-    
+
     $("#list").append(li);
-    
-} 
+}
